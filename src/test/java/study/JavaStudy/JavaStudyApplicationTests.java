4176b78ecf4ec926e825f9a59218c66aaf90951c
dmswo106 <dmswo106@naver.com>
2021-08-21 00:16:32 +0900

    실무에서 사용하는 자바공부를 하기위한 프로젝트

---
+++ src/test/java/study/JavaStudy/JavaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package study.JavaStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
